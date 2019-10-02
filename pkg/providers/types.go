c2b13c358ec8e36111e84e33dbbb1c00225d3715
dimitraz <dzuccare@redhat.com>
2019-10-02 13:09:18 +0100

    remove tests

--- pkg/providers/types.go
+++ pkg/providers/types.go
@@ -7,7 +7,7 @@ import (
 	"github.com/integr8ly/cloud-resource-operator/pkg/apis/integreatly/v1alpha1"
 )
 
-//go:generate moq -out types_moq.go . DeploymentDetails BlobStorageProvider SMTPCredentialsProvider PostgresProvider
+//go:generate moq -out types_moq.go . DeploymentDetails BlobStorageProvider SMTPCredentialsProvider
 
 type ResourceType string
 
