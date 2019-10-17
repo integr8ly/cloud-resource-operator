0599bc41a4de2297907c9f3cae7406074ee53d19
dimitraz <dzuccare@redhat.com>
2019-10-17 16:41:28 +0100

    replace underscores in config names
    Co-authored-by: ciaranRoche <croche@redhat.com>

--- pkg/providers/aws/config.go
+++ pkg/providers/aws/config.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/integr8ly/cloud-resource-operator/pkg/resources"
@@ -132,7 +133,8 @@ func buildInfraNameFromObject(ctx context.Context, c client.Client, om controlle
 	if err != nil {
 		return "", errorUtil.Wrap(err, "failed to retrieve cluster identifier")
 	}
-	return resources.ShortenString(fmt.Sprintf("%s-%s-%s", clusterId, om.Namespace, om.Name), n), nil
+	str := resources.ShortenString(fmt.Sprintf("%s-%s-%s", clusterId, om.Namespace, om.Name), n)
+	return strings.Replace(str, "_", "", -1), nil
 }
 
 func buildTimestampedInfraNameFromObject(ctx context.Context, c client.Client, om controllerruntime.ObjectMeta, n int) (string, error) {
