82669262c6639f7d871c1c8b7cda740d65e3ab53
Tsvetoslav Dimov <baskski@gmail.com>
2022-03-07 16:26:29 +0000

    test: unit tests for the sts workflow

--- pkg/providers/aws/credentials_sts_manager.go
+++ pkg/providers/aws/credentials_sts_manager.go
@@ -36,11 +36,8 @@ func NewSTSCredentialManager(client client.Client) *STSCredentialManager {
 
 //ReconcileProviderCredentials Ensure the credentials the AWS provider requires are available
 func (m *STSCredentialManager) ReconcileProviderCredentials(ctx context.Context, _ string) (*Credentials, error) {
-	secret, err := getSTSCredentialsSecret(ctx, m.Client, m.OperatorNamespace)
-	if err != nil {
-		return nil, errorUtil.Wrapf(err, "failed to get aws STS credentials secret %s", defaultSTSCredentialSecretName)
-	}
-
+	// ignoring the error here as this is validated when calling NewCredentialManager()
+	secret, _ := getSTSCredentialsSecret(ctx, m.Client, m.OperatorNamespace)
 	credentials := &Credentials{
 		RoleArn:       string(secret.Data[defaultRoleARNKeyName]),
 		TokenFilePath: string(secret.Data[defaultTokenPathKeyName]),
--- pkg/providers/aws/credentials_test.go
+++ pkg/providers/aws/credentials_test.go
@@ -2,6 +2,7 @@ package aws
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	v1 "github.com/openshift/cloud-credential-operator/pkg/apis/cloudcredential/v1"
@@ -22,63 +23,107 @@ func TestCredentialManager_ReconcileCredentials(t *testing.T) {
 	}
 	cases := []struct {
 		name                string
-		credName            string
-		credNS              string
-		entries             []v1.StatementEntry
+		client              client.Client
+		isSTS               bool
+		wantErr             bool
 		expectedAccessKeyID string
 		expectedSecretKey   string
-		client              client.Client
+		expectedRoleARN     string
+		expectedTokenPath   string
+		expectedErrMsg      string
 	}{
 		{
-			name:                "test credentials are reconciled successfully",
-			credName:            "test",
-			credNS:              "test",
-			entries:             []v1.StatementEntry{},
-			expectedAccessKeyID: "testkey",
-			expectedSecretKey:   "testsecret",
-			client: fake.NewFakeClientWithScheme(scheme, &v1.CredentialsRequest{
-				ObjectMeta: controllerruntime.ObjectMeta{
-					Name:      "test",
-					Namespace: "test",
-				},
-				Spec: v1.CredentialsRequestSpec{
-					SecretRef: v12.ObjectReference{
-						Name:      "test",
-						Namespace: "test",
-					},
-				},
-				Status: v1.CredentialsRequestStatus{
-					Provisioned: true,
-					ProviderStatus: &runtime.RawExtension{
-						Raw: []byte("{ \"user\":\"test\", \"policy\":\"test\" }"),
-					},
-				},
-			}, &v12.Secret{
-				ObjectMeta: controllerruntime.ObjectMeta{
-					Name:      "test",
-					Namespace: "test",
-				},
-				Data: map[string][]byte{
-					defaultCredentialsKeyIDName:     []byte("testkey"),
-					defaultCredentialsSecretKeyName: []byte("testsecret"),
-				},
-			}),
+			name:                "credentials are reconciled successfully",
+			client:              buildClient(scheme, false),
+			expectedAccessKeyID: "ACCESS_KEY_ID",
+			expectedSecretKey:   "SECRET_ACCESS_KEY",
+		},
+		{
+			name:              "sts credentials are reconciled successfully",
+			client:            buildClient(scheme, true, "ROLE_ARN", "TOKEN_PATH"),
+			isSTS:             true,
+			expectedRoleARN:   "ROLE_ARN",
+			expectedTokenPath: "TOKEN_PATH",
+		},
+		{
+			name:           "undefined role arn key in sts credentials secret",
+			client:         buildClient(scheme, true, "", "TOKEN_PATH"),
+			isSTS:          true,
+			wantErr:        true,
+			expectedErrMsg: fmt.Sprintf("%s key is undefined in secret %s", defaultRoleARNKeyName, defaultSTSCredentialSecretName),
+		},
+		{
+			name:           "undefined token path key in sts credentials secret",
+			client:         buildClient(scheme, true, "ROLE_ARN", ""),
+			isSTS:          true,
+			wantErr:        true,
+			expectedErrMsg: fmt.Sprintf("%s key is undefined in secret %s", defaultTokenPathKeyName, defaultSTSCredentialSecretName),
 		},
 	}
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
-			cm := NewCredentialMinterCredentialManager(tc.client)
-			//TODO: update tests to validate STS use case also
-			awsCreds, err := cm.reconcileCredentials(context.TODO(), tc.credName, tc.credNS, tc.entries)
-			if err != nil {
-				t.Fatal("unexpected error", err)
+			cm := NewCredentialManager(tc.client)
+			awsCreds, err := cm.ReconcileProviderCredentials(context.TODO(), "")
+			if tc.wantErr {
+				if !errorContains(err, tc.expectedErrMsg) {
+					t.Fatalf("unexpected error from ReconcileProviderCredentials(): %v", err)
+				}
+				return
 			}
-			if awsCreds.AccessKeyID != tc.expectedAccessKeyID {
-				t.Fatalf("unexpected access key id, expected %s but got %s", tc.expectedAccessKeyID, awsCreds.AccessKeyID)
-			}
-			if awsCreds.SecretAccessKey != tc.expectedSecretKey {
-				t.Fatalf("unexpected secret access key, expected %s but got %s", tc.expectedSecretKey, awsCreds.SecretAccessKey)
+			switch tc.isSTS {
+			case true:
+				if awsCreds.RoleArn != tc.expectedRoleARN {
+					t.Fatalf("unexpected role arn, expected %s but got %s", tc.expectedRoleARN, awsCreds.RoleArn)
+				}
+				if awsCreds.TokenFilePath != tc.expectedTokenPath {
+					t.Fatalf("unexpected toke file path, expected %s but got %s", tc.expectedTokenPath, awsCreds.TokenFilePath)
+				}
+			default:
+				if awsCreds.AccessKeyID != tc.expectedAccessKeyID {
+					t.Fatalf("unexpected access key id, expected %s but got %s", tc.expectedAccessKeyID, awsCreds.AccessKeyID)
+				}
+				if awsCreds.SecretAccessKey != tc.expectedSecretKey {
+					t.Fatalf("unexpected secret access key, expected %s but got %s", tc.expectedSecretKey, awsCreds.SecretAccessKey)
+				}
 			}
 		})
 	}
+}
+
+func buildClient(scheme *runtime.Scheme, isSTS bool, secretValues ...string) client.Client {
+	if isSTS {
+		return fake.NewFakeClientWithScheme(scheme, &v12.Secret{
+			ObjectMeta: controllerruntime.ObjectMeta{
+				Name: defaultSTSCredentialSecretName,
+			},
+			Data: map[string][]byte{
+				defaultRoleARNKeyName:   []byte(secretValues[0]),
+				defaultTokenPathKeyName: []byte(secretValues[1]),
+			},
+		})
+	}
+	return fake.NewFakeClientWithScheme(scheme, &v1.CredentialsRequest{
+		ObjectMeta: controllerruntime.ObjectMeta{
+			Name: defaultProviderCredentialName,
+		},
+		Spec: v1.CredentialsRequestSpec{
+			SecretRef: v12.ObjectReference{
+				Name: defaultProviderCredentialName,
+			},
+		},
+		Status: v1.CredentialsRequestStatus{
+			Provisioned: true,
+			ProviderStatus: &runtime.RawExtension{
+				Raw: []byte("{ \"user\":\"test\", \"policy\":\"test\" }"),
+			},
+		},
+	}, &v12.Secret{
+		ObjectMeta: controllerruntime.ObjectMeta{
+			Name: defaultProviderCredentialName,
+		},
+		Data: map[string][]byte{
+			defaultCredentialsKeyIDName:     []byte("ACCESS_KEY_ID"),
+			defaultCredentialsSecretKeyName: []byte("SECRET_ACCESS_KEY"),
+		},
+	})
 }
