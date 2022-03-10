8fa148b41090a6be716d08aefb4fd5f3cd257154
KevFan <chfan@redhat.com>
2022-03-10 17:24:24 +0000

    MGDAPI-1725 - feat: update policy script to work when running in a pod

--- scripts/sts/create-rhoam-policy.sh
+++ scripts/sts/create-rhoam-policy.sh
@@ -11,15 +11,21 @@
 # Prevents aws cli from opening editor on responses - https://github.com/aws/aws-cli/issues/4992
 export AWS_PAGER=""
 ROLE_NAME="rhoam_role"
+CLUSTER_NAME="${CLUSTER_NAME:-defaultsts}"
 
 # Gets the local aws account id
-get_account_id () {
+get_account_id() {
   local ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
   echo "$ACCOUNT_ID"
 }
 
-get_role_arn () {
+get_role_arn() {
   echo "arn:aws:iam::$(get_account_id):role/$ROLE_NAME"
+}
+
+get_cluster_id() {
+  local CLUSTER_ID=$(ocm get clusters --parameter search="name like '%$CLUSTER_NAME%'" | jq '.items[].id' -r)
+  echo "$CLUSTER_ID"
 }
 
 # Delete policy and role
@@ -28,8 +34,10 @@ aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:
 aws iam delete-role --role-name $ROLE_NAME || true
 
 # Create policy and role
+# sts:AssumeRole with iam to allow for running CRO locally with this specific iam user
+# sts:AssumeRoleWithWebIdentity with federated oidc provider to allow assuming role when running on cluster in pod
 # Allows osdCcsAdmin IAM user to assume this role
-cat << EOM > "$ROLE_NAME.json"
+cat <<EOM >"$ROLE_NAME.json"
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -38,14 +46,18 @@ cat << EOM > "$ROLE_NAME.json"
       "Principal": {
           "AWS": [
               "arn:aws:iam::$(get_account_id):user/osdCcsAdmin"
+          ],
+          "Federated": [
+              "arn:aws:iam::$(get_account_id):oidc-provider/rh-oidc.s3.us-east-1.amazonaws.com/$(get_cluster_id)"
           ]
       },
-      "Action": "sts:AssumeRole",
+      "Action": ["sts:AssumeRole", "sts:AssumeRoleWithWebIdentity"],
       "Condition": {}
     }
   ]
 }
 EOM
+
 aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document "file://$ROLE_NAME.json" || true
 # TODO - attach policy with only the required permissions by CRO
 aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AdministratorAccess || true
