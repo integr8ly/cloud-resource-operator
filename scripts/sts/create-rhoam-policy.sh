e056d050f4dc909a7ba0d4b9f6079f113ae7e457
Tsvetoslav Dimov <baskski@gmail.com>
2022-03-29 20:01:27 +0100

    chore: replace managed admin policy with minimal policy for sts usage

--- scripts/sts/create-rhoam-policy.sh
+++ scripts/sts/create-rhoam-policy.sh
@@ -11,6 +11,7 @@
 # Prevents aws cli from opening editor on responses - https://github.com/aws/aws-cli/issues/4992
 export AWS_PAGER=""
 ROLE_NAME="rhoam_role"
+MINIMAL_POLICY_NAME="${ROLE_NAME}_minimal_policy"
 CLUSTER_NAME="${CLUSTER_NAME:-defaultsts}"
 
 # Gets the local aws account id
@@ -29,8 +30,7 @@ get_cluster_id() {
 }
 
 # Delete policy and role
-# TODO - detach policy with only the required permissions by CRO
-aws iam detach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AdministratorAccess || true
+aws iam delete-role-policy --role-name $ROLE_NAME --policy-name $MINIMAL_POLICY_NAME || true
 aws iam delete-role --role-name $ROLE_NAME || true
 
 # Create policy and role
@@ -57,9 +57,109 @@ cat <<EOM >"$ROLE_NAME.json"
   ]
 }
 EOM
-
 aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document "file://$ROLE_NAME.json" || true
-# TODO - attach policy with only the required permissions by CRO
-aws iam attach-role-policy --role-name $ROLE_NAME --policy-arn arn:aws:iam::aws:policy/AdministratorAccess || true
 
+# Create the minimal required policy for CRO
+# and attach it to the RHOAM role
+cat <<EOM >"$MINIMAL_POLICY_NAME.json"
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "cloudwatch:GetMetricData",
+                "ec2:CreateRoute",
+                "ec2:DeleteRoute",
+                "ec2:DescribeAvailabilityZones",
+                "ec2:DescribeInstanceTypeOfferings",
+                "ec2:DescribeInstanceTypes",
+                "ec2:DescribeRouteTables",
+                "ec2:DescribeSecurityGroups",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVpcPeeringConnections",
+                "ec2:DescribeVpcs",
+                "elasticache:CreateReplicationGroup",
+                "elasticache:DeleteReplicationGroup",
+                "elasticache:DescribeCacheClusters",
+                "elasticache:DescribeCacheSubnetGroups",
+                "elasticache:DescribeReplicationGroups",
+                "elasticache:ModifyReplicationGroup",
+                "rds:DescribeDBInstances",
+                "rds:DescribeDBSnapshots",
+                "rds:DescribeDBSubnetGroups",
+                "rds:DescribePendingMaintenanceActions",
+                "rds:ListTagsForResource",
+                "rds:ModifyDBInstance",
+                "rds:RemoveTagsFromResource",
+                "s3:CreateBucket",
+                "s3:DeleteBucket",
+                "s3:ListAllMyBuckets",
+                "s3:ListBucket",
+                "s3:PutBucketPublicAccessBlock",
+                "s3:PutBucketTagging",
+                "s3:PutEncryptionConfiguration"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateSubnet",
+                "ec2:CreateTags",
+                "ec2:CreateVpc",
+                "ec2:CreateVpcPeeringConnection",
+                "elasticache:AddTagsToResource",
+                "elasticache:CreateCacheSubnetGroup",
+                "elasticache:CreateSnapshot",
+                "rds:AddTagsToResource",
+                "rds:CreateDBInstance",
+                "rds:CreateDBSnapshot",
+                "rds:CreateDBSubnetGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:RequestTag/red-hat-managed": "true"
+                }
+            }
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:AcceptVpcPeeringConnection",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:CreateSecurityGroup",
+                "ec2:CreateSubnet",
+                "ec2:CreateVpcPeeringConnection",
+                "ec2:DeleteSecurityGroup",
+                "ec2:DeleteSubnet",
+                "ec2:DeleteVpc",
+                "ec2:DeleteVpcPeeringConnection",
+                "elasticache:BatchApplyUpdateAction",
+                "elasticache:CreateCacheSubnetGroup",
+                "elasticache:CreateSnapshot",
+                "elasticache:DeleteCacheSubnetGroup",
+                "elasticache:DeleteSnapshot",
+                "elasticache:DescribeSnapshots",
+                "elasticache:DescribeUpdateActions",
+                "elasticache:ModifyCacheSubnetGroup",
+                "rds:DeleteDBInstance",
+                "rds:DeleteDBSnapshot",
+                "rds:DeleteDBSubnetGroup"
+            ],
+            "Resource": "*",
+            "Condition": {
+                "StringEquals": {
+                    "aws:ResourceTag/red-hat-managed": "true"
+                }
+            }
+        }
+    ]
+}
+EOM
+aws iam put-role-policy --role-name $ROLE_NAME --policy-name rhoam_role_minimal_policy --policy-document "file://$MINIMAL_POLICY_NAME.json" || true
+
+# Create the STS secret in the CRO namespace
 sed "s|ROLE_ARN|$(get_role_arn)|g" scripts/sts/sts-secret.yaml | oc apply -f - -n $NAMESPACE
