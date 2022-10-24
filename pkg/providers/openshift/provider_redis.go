944b88e65fe21ec6a04f6d003b09372133b68d16
MStokluska <mstoklus@redhat.com>
2022-10-24 18:46:09 +0100

    MGDAPI-4719 addressing failed prow checks

--- pkg/providers/openshift/provider_redis.go
+++ pkg/providers/openshift/provider_redis.go
@@ -537,10 +537,10 @@ func buildDefaultRedisPVC(r *v1alpha1.Redis) *apiv1.PersistentVolumeClaim {
 
 func int32Ptr(i int32) *int32 { return &i }
 
-// controllerutil.CreateOrUpdate without mutating the original runtime.Object provided
-func immutableCreateOrUpdate(ctx context.Context, c client.Client, o client.Object, cb func(existing runtime.Object) error) (controllerutil.OperationResult, error) {
-
-	return controllerutil.CreateOrUpdate(ctx, c, o, func() error {
-		return cb(o)
+// controllerutil.CreateOrUpdate without mutating the original client.Object provided
+func immutableCreateOrUpdate(ctx context.Context, c client.Client, o runtime.Object, cb func(existing runtime.Object) error) (controllerutil.OperationResult, error) {
+	copiedObj := o.DeepCopyObject()
+	return controllerutil.CreateOrUpdate(ctx, c, copiedObj.(client.Object), func() error {
+		return cb(copiedObj)
 	})
 }
