c6278d7d025151f91f6d4eb57808baed6ebfc1c3
Adam Cattermole <acatterm@redhat.com>
2023-01-12 13:14:20 +0000

    MGDAPI-5074 - vendor changes

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -7,12 +7,15 @@ import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // Feature holds cluster-wide information about feature gates.  The canonical name is `cluster`
+//
+// Compatibility level 1: Stable within a major release for a minimum of 12 months or 3 minor releases (whichever is longer).
+// +openshift:compatibility-gen:level=1
 type FeatureGate struct {
-	metav1.TypeMeta `json:",inline"`
-	// Standard object's metadata.
+	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
 	// spec holds user settable values for configuration
+	// +kubebuilder:validation:Required
 	// +required
 	Spec FeatureGateSpec `json:"spec"`
 	// status holds observed values from the cluster. They may not be overridden.
@@ -29,12 +32,43 @@ var (
 	// TechPreviewNoUpgrade turns on tech preview features that are not part of the normal supported platform. Turning
 	// this feature set on CANNOT BE UNDONE and PREVENTS UPGRADES.
 	TechPreviewNoUpgrade FeatureSet = "TechPreviewNoUpgrade"
+
+	// CustomNoUpgrade allows the enabling or disabling of any feature. Turning this feature set on IS NOT SUPPORTED, CANNOT BE UNDONE, and PREVENTS UPGRADES.
+	// Because of its nature, this setting cannot be validated.  If you have any typos or accidentally apply invalid combinations
+	// your cluster may fail in an unrecoverable way.
+	CustomNoUpgrade FeatureSet = "CustomNoUpgrade"
+
+	// TopologyManager enables ToplogyManager support. Upgrades are enabled with this feature.
+	LatencySensitive FeatureSet = "LatencySensitive"
 )
 
 type FeatureGateSpec struct {
+	FeatureGateSelection `json:",inline"`
+}
+
+// +union
+type FeatureGateSelection struct {
 	// featureSet changes the list of features in the cluster.  The default is empty.  Be very careful adjusting this setting.
 	// Turning on or off features may cause irreversible changes in your cluster which cannot be undone.
+	// +unionDiscriminator
+	// +optional
 	FeatureSet FeatureSet `json:"featureSet,omitempty"`
+
+	// customNoUpgrade allows the enabling or disabling of any feature. Turning this feature set on IS NOT SUPPORTED, CANNOT BE UNDONE, and PREVENTS UPGRADES.
+	// Because of its nature, this setting cannot be validated.  If you have any typos or accidentally apply invalid combinations
+	// your cluster may fail in an unrecoverable way.  featureSet must equal "CustomNoUpgrade" must be set to use this field.
+	// +optional
+	// +nullable
+	CustomNoUpgrade *CustomFeatureGates `json:"customNoUpgrade,omitempty"`
+}
+
+type CustomFeatureGates struct {
+	// enabled is a list of all feature gates that you want to force on
+	// +optional
+	Enabled []string `json:"enabled,omitempty"`
+	// disabled is a list of all feature gates that you want to force off
+	// +optional
+	Disabled []string `json:"disabled,omitempty"`
 }
 
 type FeatureGateStatus struct {
@@ -42,11 +76,13 @@ type FeatureGateStatus struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// Compatibility level 1: Stable within a major release for a minimum of 12 months or 3 minor releases (whichever is longer).
+// +openshift:compatibility-gen:level=1
 type FeatureGateList struct {
 	metav1.TypeMeta `json:",inline"`
-	// Standard object's metadata.
 	metav1.ListMeta `json:"metadata"`
-	Items           []FeatureGate `json:"items"`
+
+	Items []FeatureGate `json:"items"`
 }
 
 type FeatureGateEnabledDisabled struct {
@@ -66,25 +102,110 @@ type FeatureGateEnabledDisabled struct {
 //
 // If you put an item in either of these lists, put your area and name on it so we can find owners.
 var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
-	Default: {
-		Enabled: []string{
-			"ExperimentalCriticalPodAnnotation", // sig-pod, sjenning
-			"RotateKubeletServerCertificate",    // sig-pod, sjenning
-			"SupportPodPidsLimit",               // sig-pod, sjenning
-		},
-		Disabled: []string{
-			"LocalStorageCapacityIsolation", // sig-pod, sjenning
-		},
+	Default: defaultFeatures,
+	CustomNoUpgrade: {
+		Enabled:  []string{},
+		Disabled: []string{},
 	},
-	TechPreviewNoUpgrade: {
-		Enabled: []string{
-			"ExperimentalCriticalPodAnnotation", // sig-pod, sjenning
-			"RotateKubeletServerCertificate",    // sig-pod, sjenning
-			"SupportPodPidsLimit",               // sig-pod, sjenning
-			"CSIBlockVolume",                    // sig-storage, j-griffith
-		},
-		Disabled: []string{
-			"LocalStorageCapacityIsolation", // sig-pod, sjenning
-		},
+	TechPreviewNoUpgrade: newDefaultFeatures().
+		with("CSIMigrationAWS").             // sig-storage, jsafrane, Kubernetes feature gate
+		with("CSIMigrationGCE").             // sig-storage, fbertina, Kubernetes feature gate
+		with("CSIMigrationAzureFile").       // sig-storage, fbertina, Kubernetes feature gate
+		with("CSIMigrationvSphere").         // sig-storage, fbertina, Kubernetes feature gate
+		with("ExternalCloudProvider").       // sig-cloud-provider, jspeed, OCP specific
+		with("CSIDriverSharedResource").     // sig-build, adkaplan, OCP specific
+		with("BuildCSIVolumes").             // sig-build, adkaplan, OCP specific
+		with("NodeSwap").                    // sig-node, ehashman, Kubernetes feature gate
+		with("MachineAPIProviderOpenStack"). // openstack, egarcia (#forum-openstack), OCP specific
+		toFeatures(),
+	LatencySensitive: newDefaultFeatures().
+		with(
+			"TopologyManager", // sig-pod, sjenning
+		).
+		toFeatures(),
+}
+
+var defaultFeatures = &FeatureGateEnabledDisabled{
+	Enabled: []string{
+		"APIPriorityAndFairness",         // sig-apimachinery, deads2k
+		"RotateKubeletServerCertificate", // sig-pod, sjenning
+		"DownwardAPIHugePages",           // sig-node, rphillips
+		"PodSecurity",                    // sig-auth, s-urbaniak
 	},
+	Disabled: []string{
+		"CSIMigrationAWS",       // sig-storage, jsafrane
+		"CSIMigrationGCE",       // sig-storage, jsafrane
+		"CSIMigrationAzureFile", // sig-storage, jsafrane
+		"CSIMigrationvSphere",   // sig-storage, jsafrane
+	},
+}
+
+type featureSetBuilder struct {
+	forceOn  []string
+	forceOff []string
+}
+
+func newDefaultFeatures() *featureSetBuilder {
+	return &featureSetBuilder{}
+}
+
+func (f *featureSetBuilder) with(forceOn ...string) *featureSetBuilder {
+	f.forceOn = append(f.forceOn, forceOn...)
+	return f
+}
+
+func (f *featureSetBuilder) without(forceOff ...string) *featureSetBuilder {
+	f.forceOff = append(f.forceOff, forceOff...)
+	return f
+}
+
+func (f *featureSetBuilder) isForcedOff(needle string) bool {
+	for _, forcedOff := range f.forceOff {
+		if needle == forcedOff {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *featureSetBuilder) isForcedOn(needle string) bool {
+	for _, forceOn := range f.forceOn {
+		if needle == forceOn {
+			return true
+		}
+	}
+	return false
+}
+
+func (f *featureSetBuilder) toFeatures() *FeatureGateEnabledDisabled {
+	finalOn := []string{}
+	finalOff := []string{}
+
+	// only add the default enabled features if they haven't been explicitly set off
+	for _, defaultOn := range defaultFeatures.Enabled {
+		if !f.isForcedOff(defaultOn) {
+			finalOn = append(finalOn, defaultOn)
+		}
+	}
+	for _, currOn := range f.forceOn {
+		if f.isForcedOff(currOn) {
+			panic("coding error, you can't have features both on and off")
+		}
+		finalOn = append(finalOn, currOn)
+	}
+
+	// only add the default disabled features if they haven't been explicitly set on
+	for _, defaultOff := range defaultFeatures.Disabled {
+		if !f.isForcedOn(defaultOff) {
+			finalOff = append(finalOff, defaultOff)
+		}
+	}
+	for _, currOff := range f.forceOff {
+		finalOff = append(finalOff, currOff)
+	}
+
+	return &FeatureGateEnabledDisabled{
+		Enabled:  finalOn,
+		Disabled: finalOff,
+	}
 }
