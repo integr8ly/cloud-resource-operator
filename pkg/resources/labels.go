0f6994d951dfc246184602ddc10e3cbb32c4de54
Adam Cattermole <acatterm@redhat.com>
2023-04-19 14:30:28 +0100

    MGDAPI-5072 - label unit tests

--- pkg/resources/labels.go
+++ pkg/resources/labels.go
@@ -2,7 +2,7 @@ package resources
 
 import (
 	croType "github.com/integr8ly/cloud-resource-operator/apis/integreatly/v1alpha1/types"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -17,7 +17,7 @@ const (
 )
 
 // BuildGenericMetricLabels returns generic labels to be added to every metric
-func BuildGenericMetricLabels(objectMeta v1.ObjectMeta, clusterID, instanceID, providerName string) map[string]string {
+func BuildGenericMetricLabels(objectMeta metav1.ObjectMeta, clusterID, instanceID, providerName string) map[string]string {
 	return map[string]string{
 		LabelClusterIDKey:   clusterID,
 		LabelResourceIDKey:  objectMeta.Name,
@@ -29,7 +29,7 @@ func BuildGenericMetricLabels(objectMeta v1.ObjectMeta, clusterID, instanceID, p
 }
 
 // BuildInfoMetricLabels adds extra information to labels around resource
-func BuildInfoMetricLabels(r v1.ObjectMeta, status string, clusterID, cacheName, providerName string) map[string]string {
+func BuildInfoMetricLabels(r metav1.ObjectMeta, status string, clusterID, cacheName, providerName string) map[string]string {
 	labels := BuildGenericMetricLabels(r, clusterID, cacheName, providerName)
 	if status != "" {
 		labels[LabelStatusKey] = status
@@ -39,33 +39,33 @@ func BuildInfoMetricLabels(r v1.ObjectMeta, status string, clusterID, cacheName,
 	return labels
 }
 
-func BuildStatusMetricsLabels(r v1.ObjectMeta, clusterID, cacheName, providerName string, phase croType.StatusPhase) map[string]string {
+func BuildStatusMetricsLabels(r metav1.ObjectMeta, clusterID, cacheName, providerName string, phase croType.StatusPhase) map[string]string {
 	labels := BuildGenericMetricLabels(r, clusterID, cacheName, providerName)
 	labels[LabelStatusPhaseKey] = string(phase)
 	return labels
 }
 
 // HasLabel returns true if the label is already set
-func HasLabel(object v1.Object, key string) bool {
+func HasLabel(object metav1.Object, key string) bool {
 	labels := object.GetLabels()
 	_, ok := labels[key]
 	return ok
 }
 
 // HasLabelWithValue returns true if the label with corresponding value matches
-func HasLabelWithValue(object v1.Object, key, value string) bool {
+func HasLabelWithValue(object metav1.Object, key, value string) bool {
 	labels := object.GetLabels()
 	return labels[key] == value
 }
 
 // GetLabel retrieves a label value
-func GetLabel(object v1.Object, key string) string {
+func GetLabel(object metav1.Object, key string) string {
 	labels := object.GetLabels()
 	return labels[key]
 }
 
 // AddLabel makes sure that the provided key/value are set as a label
-func AddLabel(object v1.Object, key, value string) {
+func AddLabel(object metav1.Object, key, value string) {
 	labels := object.GetLabels()
 	if labels == nil {
 		labels = make(map[string]string)
@@ -75,7 +75,7 @@ func AddLabel(object v1.Object, key, value string) {
 }
 
 // RemoveLabel makes sure that the provided label is removed
-func RemoveLabel(object v1.Object, key string) {
+func RemoveLabel(object metav1.Object, key string) {
 	labels := object.GetLabels()
 	delete(labels, key)
 	object.SetLabels(labels)
