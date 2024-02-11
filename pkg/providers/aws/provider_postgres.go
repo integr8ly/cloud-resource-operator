b046ec516b03c80bebe6e0acdf02016f292d384a
Valery Mogilevsky <vmogilev@redhat.com>
2024-02-11 12:53:27 +0200

    MGDAPI-6233 - Bump k8s.io/apimachinery to v0.26.5

--- pkg/providers/aws/provider_postgres.go
+++ pkg/providers/aws/provider_postgres.go
@@ -44,7 +44,7 @@ const (
 	defaultAwsDBInstanceClass            = "db.t3.small"
 	defaultAwsDeleteAutomatedBackups     = true
 	defaultAwsEngine                     = "postgres"
-	defaultAwsEngineVersion              = "13.13"
+	defaultAwsEngineVersion              = "13.8"
 	defaultAwsIdentifierLength           = 40
 	defaultAwsMaxAllocatedStorage        = 100
 	defaultAwsMultiAZ                    = true
@@ -62,7 +62,7 @@ const (
 )
 
 var (
-	defaultSupportedEngineVersions = []string{"13.13", "13.12", "13.11"}
+	defaultSupportedEngineVersions = []string{"13.8", "13.4", "10.18", "10.16", "10.15", "10.13", "10.6", "9.6", "9.5"}
 	healthyAWSDBInstanceStatuses   = []string{
 		"backtracking",
 		"available",
