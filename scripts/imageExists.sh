4bcd564ac6401d8bb8337f7ba4ec2d2939299fe7
Valery Mogilevsky <vmogilev@redhat.com>
2024-08-20 13:56:05 +0300

    MGDAPI-6423 - fix imageExists.sh

--- scripts/imageExists.sh
+++ scripts/imageExists.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-CONTAINER_ENGINE ?= podman
+
+CONTAINER_ENGINE="${CONTAINER_ENGINE:-podman}"
 
 if ${CONTAINER_ENGINE} pull ${IMAGE_TO_SCAN} > /dev/null; then
   echo "already exist"
