4971a36b3adbef17a63534aa5189bf779e6b32ab
Austin Cunningham <aucunnin@redhat.com>
2026-01-07 14:06:39 +0000

    MGDAPI-6549 bump the postgres engine version to 15.15

--- pkg/providers/aws/provider_postgres.go
+++ pkg/providers/aws/provider_postgres.go
@@ -45,7 +45,7 @@ const (
 	defaultAwsDBInstanceClass            = "db.t3.small"
 	defaultAwsDeleteAutomatedBackups     = true
 	defaultAwsEngine                     = "postgres"
-	defaultAwsEngineVersion              = "13.18"
+	defaultAwsEngineVersion              = "15.15"
 	defaultAwsIdentifierLength           = 40
 	defaultAwsMaxAllocatedStorage        = 100
 	defaultAwsMultiAZ                    = true
@@ -65,7 +65,7 @@ const (
 )
 
 var (
-	defaultSupportedEngineVersions = []string{"13.13", "13.12", "13.11"}
+	defaultSupportedEngineVersions = []string{"15.15", "13.18", "13.13", "13.12", "13.11"}
 	healthyAWSDBInstanceStatuses   = []string{
 		"backtracking",
 		"available",
